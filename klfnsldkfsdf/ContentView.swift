be8b9c5755c2ded9358dff1549837afe9b8d23be
Nikash Ramsorrun <yogesh-nikash.ramsorrun@octoenergy.com>
2020-09-04 12:42:45 +0100

    Initial Commit

---
+++ klfnsldkfsdf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  klfnsldkfsdf
+//
+//  Created by Yogesh N Ramsorrrun on 04/09/2020.
+//  Copyright © 2020 Yogesh N Ramsorrrun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
