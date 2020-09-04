c52b28cf603bfe87dff3581e53f0675bc535180d
Nikash Ramsorrun <yogesh-nikash.ramsorrun@octoenergy.com>
2020-09-04 14:08:56 +0100

    3rd Onboarding commit

--- klfnsldkfsdf/ContentView.swift
+++ klfnsldkfsdf/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("He, Wo  dsfsfgsfrld!")
+        VStack {
+            Text("Hellodflknasdfknsfdns;dfsdsfsfsfsfsdfsfd, World!")
+        }
     }
 }
 
